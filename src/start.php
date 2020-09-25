ddefad81e67ee8be2dfd61631640ba7d615a6679
Grupo Facela <facela.shop.2020@gmail.com>
2020-09-25 09:12:59 -0600

    Metodo reload

--- src/start.php
+++ src/start.php
@@ -9,13 +9,12 @@
 	$io->on('connection', function ($socket) use ($io) {
 		$id_user;
 	    $socket->on('changeRoom', function ($id) use ($io) {
-	    	echo "Se obtuvo el ID: ".$id;
 	        $id_user = $id;
 	        $io->join($id_user);
 	    });
-	    // $socket->on('reload', function ($msg) use ($io) {
-	    // 	$io->to($id_user)->emit("message", "reload");
-	    // });
+	    $socket->on('reload', function ($msg) use ($io) {
+	    	$io->to($id_user)->emit("message", "reload");
+	    });
 	});
 
 	Worker::runAll();
